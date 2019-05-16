4a2929d3af083ba9bfafaf8a9ad615fbd96b33a4
Mark R Pothecary <markpothers@hotmail.com>
2019-05-16 16:29:45 -0500

    first commit

---
+++ public/precache-manifest.526cc231d84e6f62db485a83ef47185b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a3c9fdd9692b7341eec60cc880a780",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9229063590a17999bb3d",
+    "url": "/static/js/2.00029d87.chunk.js"
+  },
+  {
+    "revision": "40c62dd6c2ad2afaded1",
+    "url": "/static/js/main.e8f9ff9e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
